e13c0aeb52316ac10de409b2660ba90a904556a3
abubakkar-mee <78528078+abubakkar-mee@users.noreply.github.com>
2022-12-13 18:06:58 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const ul = window.document.getElementById("ul");
+const menuToggler = window.document.getElementById("menuToggler")
+
+
+menuToggler.addEventListener("click", () =>{
+    ul.classList.toggle("toggle")
+})
